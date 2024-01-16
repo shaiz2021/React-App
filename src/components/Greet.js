c4b7fb3e1cf9cda77d8d7bdada08edd32ad79eb2
Shahzaib <100790821+shaiz20@users.noreply.github.com>
2024-01-16 12:24:27 +0500

    Updated some new files

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,5 +1,8 @@
 import React from 'react'
-function Greet(){
-    return <h1> Hello it's beginning of my React World </h1>
-}
+//function Greet(){
+//    return <h1> Hello it's beginning of my React World </h1>
+//}
+
+const Greet = () => <h1> Hello it's proper starting of my React World </h1>
+
 export default Greet
